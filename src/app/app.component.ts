dc232c6e2ffb317429f9182d0737c6f5b2a90de2
Detelin Zlatev <detelin.zlatev@cayetanogaming.com>
2017-03-06 15:09:23 +0200

    Added push notifications subscriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
-import { Component} from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
 
-import { Platform} from 'ionic-angular';
+import { Platform, AlertController, Nav} from 'ionic-angular';
 
-import { StatusBar, Splashscreen } from 'ionic-native';
+import { StatusBar, Splashscreen, Push } from 'ionic-native';
 
 import { CategoriesPage } from '../pages/categories/categories';
+import { ProductPage } from '../pages/product/product';
+
+import {EmailsService} from '../providers/emails-service';
 
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: 'app.html',
+  providers: [EmailsService]
 })
 export class MyApp {
 
+  @ViewChild(Nav) nav: Nav;
   rootPage: any = CategoriesPage;
 
-  constructor(public platform: Platform) {
+  constructor(public platform: Platform,
+              public alertCtrl: AlertController,
+	      public emailService: EmailsService) {
     this.initializeApp();
   }
 
@@ -24,6 +31,63 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+      this.initPushNotification();
+    });
+  }
+
+
+  initPushNotification(){
+    if (!this.platform.is('cordova')) {
+      console.warn("Push notifications not initialized. Cordova is not available - Run in physical device");
+      return;
+    }
+    let push = Push.init({
+      android: {
+        senderID: "869504917457",
+	topics: ['newProduct']
+      },
+      ios: {
+        alert: "true",
+        badge: false,
+        sound: "true"
+      },
+      windows: {}
+    });
+
+    push.on('registration', (data) => {
+      console.log("device token ->", data.registrationId);
+      this.emailService.addDeviceToken(data.registrationId);
+    });
+    push.on('notification', (data) => {
+      console.log('message', data.message);
+      let self = this;
+      //if user using app and push notification comes
+      if (data.additionalData.foreground) {
+        // if application open, show popup
+        let confirmAlert = this.alertCtrl.create({
+          title: 'Нов продукт',
+          message: data.message,
+          buttons: [{
+            text: 'Пропусни',
+            role: 'cancel'
+          }, {
+            text: 'Виж',
+            handler: () => {
+              //TODO: Your logic here
+              self.nav.push(ProductPage, {id: data.message});
+            }
+          }]
+        });
+        confirmAlert.present();
+      } else {
+        //if user NOT using app and push notification comes
+        //TODO: Your logic on click of push notification directly
+        self.nav.push(ProductPage, {id: data.message});
+        console.log("Push notification clicked");
+      }
+    });
+    push.on('error', (e) => {
+      console.log(e.message);
     });
   }
 }
